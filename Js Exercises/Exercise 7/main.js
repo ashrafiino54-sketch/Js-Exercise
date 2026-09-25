2e77bc0930d751efe155b27ab86c387cabb7921d
Ashraf <ashrafiino54@gmail.com>
2026-09-25 22:14:24 +0300

    Add Exercise 7

---
+++ Js Exercises/Exercise 7/main.js
@@ -0,0 +1,25 @@
+
+// Ex7 Arrays
+
+let colors = ["blue","green", "Red"];
+console.log(colors);
+//add the end of the color
+console.log("add the end of the color ");
+colors.push("black")
+console.log(colors);
+
+//removing the first color 
+console.log("removing the first color ");
+colors.shift();
+console.log(colors);
+
+
+
+
+
+
+
+
+
+
+
